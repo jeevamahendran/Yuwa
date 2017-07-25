d0fe8526396d2e8036112bc57cc93c4469b88e2f
Jeeva Mahendran <mahendranjeeva@gmail.com>
2017-07-25 16:42:13 +0530

    Add project files.

---
+++ Yuwa/Yuwa.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Yuwa.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Yuwa.App());
+        }
+    }
+}
